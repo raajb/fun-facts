b843cc8bc35b28e83bebda3b4fa8b931ccd86141
raajb <raaj@metabahn.com>
2017-01-01 16:31:17 -0600

    Created the Basic layout

--- FunFacts/FunFacts/ViewController.swift
+++ FunFacts/FunFacts/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        funFactLabel.text = "An Interesting fact!"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFact() {
+        funFactLabel.text = "Another interesting fact!"
+    }
 
 }
 
