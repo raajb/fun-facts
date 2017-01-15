bce41f51e1edea2197028a06b5a4edd26ee30c8d
raajb <raaj@metabahn.com>
2017-01-15 17:47:03 -0600

    Added random background and tint color feature

--- FunFacts/FunFacts/ViewController.swift
+++ FunFacts/FunFacts/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
     let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +30,9 @@ class ViewController: UIViewController {
 
     @IBAction func showFact() {
         funFactLabel.text = factProvider.randomFact()
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
     }
 
 }
